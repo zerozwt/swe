3ad31678a7a7975cc5fce96da964138c448a8371
zerozwt <31161284+zerozwt@users.noreply.github.com>
2023-06-16 05:00:32 +0800

    add logid middleware

---
+++ logid.go
@@ -0,0 +1,28 @@
+package swe
+
+import (
+	"fmt"
+	"sync/atomic"
+	"time"
+)
+
+var CtxKeyLogID string = "ctx_logid"
+var httpHeaderLogIDKey string = "X-Logid"
+var logidIncr atomic.Int64
+
+func InitLogID(ctx *Context) {
+	logid := ctx.Request.Header.Get(httpHeaderLogIDKey)
+	if len(logid) == 0 {
+		// generate logid
+		ts := time.Now().Unix()
+		low := logidIncr.Add(1)
+		logid = fmt.Sprint(ts<<20 | (low & 0xFFFFF))
+	}
+	ctx.Put(CtxKeyLogID, logid)
+	ctx.Next()
+}
+
+func CtxLogID(ctx *Context) string {
+	logid, _ := CtxValue[string](ctx, httpHeaderLogIDKey)
+	return logid
+}
